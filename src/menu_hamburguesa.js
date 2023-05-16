49ab389720fb082fb1f423222cd6070cdb2d5f58
Jxanmont0ya <juanpablomp7@gmail.com>
2023-05-16 09:42:05 -0400

    arreglos en el menu hamburgusa

--- src/menu_hamburguesa.js
+++ src/menu_hamburguesa.js
@@ -3,18 +3,18 @@
 const iconoMenu = document.querySelector("#icono-menu"),
   menu = document.querySelector("#menu");
 
-  iconoMenu.addEventListener("click" , (e) => {
+iconoMenu.addEventListener("click", (e) => {
 
   //Alterno estilos para el menu y body//
-  menu.classList.toggle("active")
-  document.body.classList.toggle("opacity");
+  menu.classList.toggle("activar")
+  document.body.classList.toggle("opa");
 
   //Alterno   su tributo "src"  para el icono del  menu //
   const rutaActual = e.target.getAttribute("src");
 
-  if(rutaActual == "img/menu hamburguesa.png") {
+  if (rutaActual == "img/menu hamburguesa.png") {
     e.target.setAttribute("src", "img/menu hamburguesa.png");
-  }else{
+  } else {
     e.target.setAttribute("src", "img/menu hamburguesa.png");
   }
 
